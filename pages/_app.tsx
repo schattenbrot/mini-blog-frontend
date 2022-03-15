f563b5f1ae6f5895b9fca8f6ce546fc0c7fd622b
Schattenbrot <aaron.machill@web.de>
2022-03-15 16:58:55 +0100

    add backend url env variable support for axios

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,10 @@ import axios from "axios";
 import Layout from "../layouts/Layout";
 import "../styles/globals.scss";
 
-axios.defaults.baseURL = process.env.API_BASE_URL
-  ? process.env.API_BASE_URL
-  : "http://localhost:4000";
+axios.defaults.baseURL =
+  process.env.NEXT_PUBLIC_BACKEND_URL != undefined
+    ? process.env.NEXT_PUBLIC_BACKEND_URL
+    : "http://localhost:4000";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
