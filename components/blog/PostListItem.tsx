f6c39c7264ef9c8a3239af21a73bc1f4c2477d7e
Schattenbrot <aaron.machill@web.de>
2022-03-16 01:49:06 +0100

    show one post by its ID

--- components/blog/PostListItem.tsx
+++ components/blog/PostListItem.tsx
@@ -1,15 +1,29 @@
+import { useRouter } from "next/router";
+import { MouseEventHandler } from "react";
 import { PostType } from "../../models/models";
 import styles from "../../styles/components/blog/PostListItem.module.scss";
 
 export type PostListItemType = {
   post: PostType;
+  singlePost?: boolean;
 };
 
 const PostListItem: React.FC<PostListItemType> = (props) => {
+  const router = useRouter();
   const { id, title, text, creator, created_at, updated_at } = props.post;
 
+  const cardStyle = props.singlePost ? "card" : "card hover-scale";
+
+  const onClickHandler: MouseEventHandler<HTMLLIElement> = (event) => {
+    if (props.singlePost) {
+      return;
+    }
+
+    router.push("/blog/" + id);
+  };
+
   return (
-    <li className={styles.card}>
+    <li className={styles[cardStyle]} onClick={onClickHandler}>
       <div>
         <h2 className={styles["card__title"]}>{title}</h2>
         <p className={styles["card__text"]}>{text}</p>
