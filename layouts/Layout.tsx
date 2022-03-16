c75ea17ca196029697b552d02856a25ae1ec77a9
Schattenbrot <aaron.machill@web.de>
2022-03-16 01:25:29 +0100

    uwu

--- layouts/Layout.tsx
+++ layouts/Layout.tsx
@@ -1,10 +1,51 @@
 import type { NextComponentType } from "next";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 import Navbar from "../components/base/Navbar";
 import styles from "../styles/layouts/layout.module.scss";
 
 const Layout: NextComponentType = ({ children }) => {
+  const [userInputs, setUserInputs] = useState<string>("");
+  const [showEasterEgg, setShowEasterEgg] = useState<boolean>(false);
+
+  useEffect(() => {
+    const lastInputs = userInputs.slice(
+      userInputs.length - 3,
+      userInputs.length
+    );
+
+    if (lastInputs === "uwu") {
+      setShowEasterEgg(true);
+    }
+
+    if (userInputs.length >= 20) {
+      setUserInputs(lastInputs);
+    }
+  }, [userInputs]);
+
+  const keyPressHandler = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    setUserInputs((input) => input + event.key);
+  };
+
+  const closeEasterEgg: React.MouseEventHandler<HTMLDivElement> = (event) => {
+    setUserInputs("");
+    setShowEasterEgg(false);
+  };
+
   return (
-    <div className={styles.grid}>
+    <div className={styles.grid} tabIndex={0} onKeyPress={keyPressHandler}>
+      {showEasterEgg ? (
+        <div className={styles.easterContainer} onClick={closeEasterEgg}>
+          <Image
+            src='/astolfo.png'
+            alt='astolfo it is'
+            width='780px'
+            height='1033px'
+          />
+        </div>
+      ) : (
+        ""
+      )}
       <header className={styles.header}>
         <Navbar />
       </header>
