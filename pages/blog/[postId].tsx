a0cda301b3a0ad4c45a510c2e6c5552a36405413
Schattenbrot <aaron.machill@web.de>
2022-03-16 01:25:13 +0100

    added create-post page and view for a single page

---
+++ pages/blog/[postId].tsx
@@ -0,0 +1,67 @@
+import axios from "axios";
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
+import Head from "next/head";
+import { ParsedUrlQuery } from "querystring";
+import Button from "../../components/base/Button";
+import PostListItem from "../../components/blog/PostListItem";
+import { PostType } from "../../models/models";
+import styles from "../../styles/pages/blog/PostDetails.module.scss";
+
+export type PostDetailsProps = {
+  selectedPost: PostType;
+};
+
+const PostDetailsPage: NextPage<PostDetailsProps> = (props) => {
+  return (
+    <>
+      <Head>
+        <title>uwuBlog - Post</title>
+        <meta name='description' content='Create a new post' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <div className={styles.container}>
+        <PostListItem post={props.selectedPost} />
+        <div className={styles.controls}>
+          <Button type='button' className='btn ok'>
+            Yeah uwu
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+interface IParams extends ParsedUrlQuery {
+  postId: string;
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const { postId } = context.params as IParams;
+
+  const res = await axios.get(`/v1/posts/${postId}`);
+
+  if (!res) {
+    console.error("fuck");
+  }
+
+  const post: PostType = res.data;
+
+  return {
+    props: {
+      selectedPost: post,
+    },
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const res = await axios.get(`/v1/posts`);
+  const posts: PostType[] = res.data;
+  const paths = posts.map((post) => ({ params: { postId: post.id } }));
+
+  return {
+    paths: paths,
+    fallback: "blocking",
+  };
+};
+
+export default PostDetailsPage;
--- pages/blog/create-post.tsx
+++ pages/blog/create-post.tsx
@@ -1,15 +1,89 @@
 import { NextPage } from "next";
 import Head from "next/head";
+import styles from "../../styles/pages/blog/CreatePost.module.scss";
+import useInput from "../../hooks/useInput";
+import Button from "../../components/base/Button";
+import { validateText, validateTitle } from "../../helpers/validation";
+import axios from "axios";
+import { useRouter } from "next/router";
 
 const CreatePostPage: NextPage = () => {
+  const router = useRouter();
+  const { value: title, bind: bindTitle, reset: resetTitle } = useInput("");
+  const { value: text, bind: bindText, reset: resetText } = useInput("");
+
+  const submitHandler: React.FormEventHandler = async (event) => {
+    event.preventDefault();
+    console.log("clicked");
+
+    // validate Title and Text
+    if (!validateTitle(title) && !validateText(text)) {
+      console.log("ups");
+      return;
+    }
+
+    const payload = {
+      title,
+      text,
+    };
+
+    const response = await axios.post("/v1/posts", payload, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      withCredentials: true,
+    });
+
+    if (!response) {
+      console.log("Something went wrong!");
+    }
+
+    if (response.status === 201) {
+      router.push("/blog");
+      return;
+    }
+  };
+
+  const resetHandler: React.FormEventHandler = (event) => {
+    event.preventDefault();
+    resetTitle();
+    resetText();
+  };
+
   return (
     <>
       <Head>
-        <title>Create Post</title>
+        <title>uwuBlog - Create Post</title>
         <meta name='description' content='Create a new post' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <div>Create Post</div>
+      <main className={styles.container}>
+        <h1>Create Post</h1>
+        <form onSubmit={submitHandler} onReset={resetHandler}>
+          <div className={styles["input-element"]}>
+            <label htmlFor='title'>Title</label>
+            <input type='text' name='title' id='title' {...bindTitle} />
+          </div>
+          <div className={styles["input-element"]}>
+            <label htmlFor='text'>Text</label>
+            <textarea
+              name='text'
+              id='text'
+              rows={4}
+              maxLength={700}
+              {...bindText}
+            />
+          </div>
+          <div className={styles.control}>
+            <Button type='reset' className='danger'>
+              Reset
+            </Button>
+            <Button type='submit' className='ok'>
+              Create
+            </Button>
+          </div>
+        </form>
+      </main>
     </>
   );
 };
