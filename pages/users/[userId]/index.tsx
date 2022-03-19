93fe6048b28f6296f68ac40d06695694ba6b6559
Schattenbrot <aaron.machill@web.de>
2022-03-19 20:01:03 +0100

    add edit user page

--- pages/users/[userId]/index.tsx
+++ pages/users/[userId]/index.tsx
@@ -1,18 +1,25 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import Head from "next/head";
+import { useRouter } from "next/router";
 import { ParsedUrlQuery } from "querystring";
-import { useEffect, useState } from "react";
+import {
+  FormEventHandler,
+  MouseEventHandler,
+  useEffect,
+  useState,
+} from "react";
 import Button from "../../../components/base/Button";
 import UserListItem from "../../../components/users/UserListItem";
 import { UserType } from "../../../models/models";
-import styles from "../../styles/pages/users/UserDetails.module.scss";
+import styles from "../../../styles/pages/users/UserDetails.module.scss";
 
 export type UserDetailsProps = {
   selectedUserId: string;
 };
 
 const UserDetailsPage: NextPage<UserDetailsProps> = (props) => {
+  const router = useRouter();
   const [selectedUser, setSelectedUser] = useState<UserType>();
 
   useEffect(() => {
@@ -27,6 +34,26 @@ const UserDetailsPage: NextPage<UserDetailsProps> = (props) => {
     })();
   }, [props.selectedUserId]);
 
+  const submitHandler: FormEventHandler = (event) => {
+    event.preventDefault();
+
+    router.push(`/users/${selectedUser!.id}/edit`);
+  };
+
+  const deleteHandler: MouseEventHandler<Element> = async () => {
+    try {
+      await axios.delete(`/users/${selectedUser!.id}`);
+
+      router.push(`/`);
+    } catch (error: unknown | AxiosError) {
+      if (axios.isAxiosError(error)) {
+        console.error("This is an axios error: Delete failed");
+      } else {
+        console.error("This is not an axios error: Delete failed");
+      }
+    }
+  };
+
   return (
     <>
       <Head>
@@ -35,12 +62,20 @@ const UserDetailsPage: NextPage<UserDetailsProps> = (props) => {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <div className={styles.container}>
-        {selectedUser && <UserListItem user={selectedUser} />}
-        <div className={styles.controls}>
-          <Button type='button' className='btn ok'>
-            Yeah uwu
-          </Button>
-        </div>
+        {selectedUser && (
+          <>
+            <UserListItem user={selectedUser} />
+            <form className={styles.controls} onSubmit={submitHandler}>
+              <Button type='button' className='danger' onClick={deleteHandler}>
+                Delete
+              </Button>
+              <Button type='submit' className='btn ok'>
+                Edit
+              </Button>
+            </form>
+          </>
+        )}
+        {!selectedUser && <h1>No user found</h1>}
       </div>
     </>
   );
