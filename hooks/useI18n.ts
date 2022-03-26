6c3dbe5ec23ab05da138c9d550aafb833cb9ecc3
Schattenbrot <aaron.machill@web.de>
2022-03-26 21:39:07 +0100

    better page distinguishment even for dynamic pages

--- hooks/useI18n.ts
+++ hooks/useI18n.ts
@@ -2,6 +2,22 @@ import english from "../i18n/english";
 import german from "../i18n/german";
 
 const useI18n = (locale: string | undefined, page: string) => {
+  const pageSplit = page.split("/");
+  console.log(pageSplit);
+  if (pageSplit.length > 2) {
+    switch (pageSplit[1]) {
+      case "blog": {
+        console.log("blog case triggered");
+        if (pageSplit[2] !== "create") page = "postDetails";
+        break;
+      }
+      case "users": {
+        page = "userDetails";
+        if (pageSplit[3] && pageSplit[3] === "edit") page = "editUser";
+      }
+    }
+  }
+
   switch (locale) {
     case "de":
       return getContent(german, page);
