36f8c07cd86021a5e05d23fa5e30c82bf996a4be
Schattenbrot <aaron.machill@web.de>
2022-03-27 20:52:53 +0200

    add next-i18next translation for about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,14 +1,21 @@
-import { NextPage } from "next";
-import { useRouter } from "next/router";
-import useI18n from "../hooks/useI18n";
-import { AboutPageTextType } from "../i18n/types";
+import { GetStaticProps, NextPage } from "next";
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 import styles from "../styles/pages/About.module.scss";
 
 const About: NextPage = () => {
-  const { locale, asPath } = useRouter();
-  const lang: AboutPageTextType = useI18n(locale, asPath);
-  return <div className={styles.test}>{lang.content}</div>;
+  const { t } = useTranslation();
+  return <div className={styles.test}>{t("about:content")}</div>;
+};
+
+export const getStaticProps: GetStaticProps = async ({ locale }) => {
+  if (locale) locale = "en";
+  return {
+    props: {
+      ...(await serverSideTranslations(locale!, ["about"])),
+    },
+  };
 };
 
 export default About;
